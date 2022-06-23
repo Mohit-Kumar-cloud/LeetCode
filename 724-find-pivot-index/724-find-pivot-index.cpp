8ca82000b82bc03262292d932779866f4326ca53
Mohit Kumar <dashingmk16@gmail.com>
2022-06-23 10:15:58 +0500

    Time: 47 ms (25.66%), Space: 31.1 MB (54.48%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int ls = 0;
+        int rs = sum;
+        for(int i=0;i<nums.size();i++)
+        {
+            rs-=nums[i];
+            if(rs==ls)
+            {
+                return i;
+            }
+            ls+=nums[i];
+        }
+        return -1;
+    }
+};
